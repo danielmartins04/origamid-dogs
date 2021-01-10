9c04e5888bc31852c7be579a51dbd5ced155300a
daniel martins <freitasdaniel39@gmail.com>
2021-01-10 10:10:06 -0300

    estilizando comentários

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,15 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
     const [comments, setComments] = useState(() => props.comments);
+    const commentsSection = useRef(null);
     const { login } = useContext(UserContext);
+
+    useEffect(() => {
+        commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+    }, [comments]);
 
     return (
         <>
-            <ul className={styles.comment}>
+            <ul ref={commentsSection} className={styles.comments}>
                 {comments.map(comment => <li key={comment.comment_ID}>
                     <b>{comment.comment_author}: </b>
                     <span>{comment.comment_content}</span>
@@ -20,4 +25,4 @@ const PhotoComments = (props) => {
     );
 }
 
-export default PhotoComments
+export default PhotoComments;
