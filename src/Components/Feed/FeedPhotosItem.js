146c7c65845432f158738d46c5a9b2ab981390a4
daniel martins <freitasdaniel39@gmail.com>
2021-01-11 18:48:13 -0300

    criando efeito de skeleton

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Image from '../Helper/Image';
 import styles from './FeedPhotosItem.module.css';
 
-const FeedPhotosItem = ({photo, setModalPhoto}) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
     function handleClick() {
         setModalPhoto(photo);
     }
 
     return (
         <li className={styles.photo} onClick={handleClick}>
-            <img src={photo.src} alt={photo.title} />
+            <Image src={photo.src} alt={photo.title} />
             <span className={styles.vizualizacao}>{photo.acessos}</span>
         </li>
     );
