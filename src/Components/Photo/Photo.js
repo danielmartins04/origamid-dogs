621a5cd80a2e8ef6f74ef7bb8ab5a5c9039ac7be
daniel martins <freitasdaniel39@gmail.com>
2021-01-15 20:54:35 -0300

    implementando paginas para mostrar foto especifica

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { PHOTO_GETBYID } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import PhotoContent from './PhotoContent';
 
 const Photo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+    const { id } = useParams();
+    const { data, error, request } = useFetch();
+
+    useEffect(() => {
+        const { url, options } = PHOTO_GETBYID(id);
+        const { response } = request(url, options);
+    }, [request, id]);
+
+    if (error) return <Error error={error} />   
+    if (data) 
+        return (
+            <section className="container mainContainer">
+                <PhotoContent single={true} data={data} />
+            </section>
+        );
+    
+    else return null;
+    
 };
 
 export default Photo;
--- src/api.js
+++ src/api.js
@@ -73,6 +73,13 @@ export function PHOTOS_GET({page, total, user}) {
     }; 
 }
 
+export function PHOTO_GETBYID(id) {
+    return {
+        url: API_URL + `/api/photo/${id}`,
+    }; 
+}
+
+
 export function PHOTO_GET(id) {
     return {
         url: API_URL + `/api/photo/${id}`,
